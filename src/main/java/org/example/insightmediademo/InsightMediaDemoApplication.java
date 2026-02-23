1e82ddcedf8f862c79cb907e02e583a2dfdb384c
UT <bluesky201101@gmail.com>
2026-02-23 15:24:55 +0800

    Update: Database connection successful

--- src/main/java/org/example/insightmediademo/InsightMediaDemoApplication.java
+++ src/main/java/org/example/insightmediademo/InsightMediaDemoApplication.java
@@ -3,12 +3,27 @@ package org.example.insightmediademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @SpringBootApplication
 public class InsightMediaDemoApplication {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 
         SpringApplication.run(InsightMediaDemoApplication.class, args);
+    }*/
+    public static void main(String[] args) {
+        var context = SpringApplication.run(InsightMediaDemoApplication.class, args);
+
+        // 直接從 Spring context 拿 DataSource
+        DataSource dataSource = context.getBean(DataSource.class);
+
+        try (Connection conn = dataSource.getConnection()) {
+            System.out.println("資料庫連線成功!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
